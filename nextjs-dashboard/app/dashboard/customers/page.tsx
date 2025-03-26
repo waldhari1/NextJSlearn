b1909e2d7b47d848430e98715383d90c14366410
Ifeanyi RG <ifeanyi@waltoria.io>
2025-03-26 19:45:32 +0000

    updated the invoices UI for this app

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,8 +1,3 @@
-export default function page() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Customers</h1>
-      <p className="mt-4 text-lg">Welcome to the customers page!</p>
-    </div>
-  );
-}
+export default function Page() {
+    return <p>Customers Page</p>;
+  }
