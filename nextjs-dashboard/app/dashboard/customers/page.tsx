49912022e6d23c811af20e618e05a2812cb6bb29
Ifeanyi RG <ifeanyi@waltoria.io>
2025-03-26 17:18:46 +0000

    learning continued

---
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function page() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Customers</h1>
+      <p className="mt-4 text-lg">Welcome to the customers page!</p>
+    </div>
+  );
+}
